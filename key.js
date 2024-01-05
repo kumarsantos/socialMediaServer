46696ba5bd687b899e2e4bfe6a1929e2dafb7fb0
SANTOSH KUMAR SAH <70064162+kumarsantos@users.noreply.github.com>
2024-01-05 18:49:09 +0530

    Update key.js

--- key.js
+++ key.js
@@ -1,8 +1,6 @@
 module.exports = {
   MONGODBURI:
-    "mongodb+srv://santoshkumarsah:0VrUVNACCWId5TVc@cluster0.cejei.mongodb.net/Instagram_clone?retryWrites=true&w=majority",
-  JSON_SECRET: "zasgdkjkhwerufuehihrihhrhhjhrhjhjrujhiehuhu", //random value of JSON_SECRET
+    "mongodb+srv://username:password@cluster0.cejei.mongodb.net/Instagram_clone?retryWrites=true&w=majority",
+  JSON_SECRET: "random token", //random value of JSON_SECRET
 };
 
-//MongoDB passwor=0VrUVNACCWId5TVc
-//Username=santoshkumarsah
