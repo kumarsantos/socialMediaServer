30ccc50f87df39f7877ffaf44c9f98778dba478c
SANTOSH KUMAR SAH <70064162+kumarsantos@users.noreply.github.com>
2024-01-05 18:44:55 +0530

    Delete key.js

--- key.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-     MONGODBURI : "mongodb+srv://'username':'password'@cluster0.cejei.mongodb.net/Instagram_clone?retryWrites=true&w=majority",
-     JSON_SECRET:"zasgdkjkhwerufuehihrihhrhhjhrhjhjrujhiehuhu"//random value of JSON_SECRET
-}
-
-//MongoDB passwor=0VrUVNACCWId5TVc
-//Username=santoshkumarsah
